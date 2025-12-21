edde9476c9e7fbf8358458c9f12c4090ee4793b0
Yugisaki <franekstrocen@gmail.com>
2025-12-21 19:21:34 +0100

    cli clock

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("hello world")
+import machine
+from utime import sleep
+
+led = machine.Pin("LED", machine.Pin.OUT)
+led.on()
+sleep(.25)
+led.off()
+
+import network
+
+ssid = "You wifi name"
+password = "your wifi password"
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid,password)
+
+sleep(.2)
+while not wlan.isconnected():
+    sleep(.3)
+
+import ntptime, time
+ntptime.host = "pool.ntp.org"
+ntptime.settime()
+sleep(2)
+
+while (True):
+    try:
+        current_time = time.localtime()
+        year, month, day, hour, minute, sec, milsec, nanosec = current_time
+        print(f"h: {hour + 1} min: {minute} sec: {sec}")
+        sleep(1)
+
+    except KeyboardInterrupt:
+        print("bye")
+        break
