8b99241e27f21b4a36dd2ff9f8a5e043599cfcea
Yugisaki <franekstrocen@gmail.com>
2025-12-21 20:29:19 +0100

    Display time verticly

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import machine
 from utime import sleep
 
+# Led for init
 led = machine.Pin("LED", machine.Pin.OUT)
 led.on()
 sleep(.25)
 led.off()
 
+# Network
 import network
 
 ssid = "You wifi name"
@@ -19,17 +21,45 @@ sleep(.2)
 while not wlan.isconnected():
     sleep(.3)
 
-import ntptime, time
+import ntptime, time # set time server
 ntptime.host = "pool.ntp.org"
 ntptime.settime()
 sleep(2)
 
+import Drivers
+
+epd = Drivers.EPD_2in9()
+tp = Drivers.ICNT86()
+icnt_dev = Drivers.ICNT_Development()
+icnt_old = Drivers.ICNT_Development()
+
+epd.init()
+tp.ICNT_Init()
+
+
+epd.fill(0xff)
+epd.display_Base(epd.buffer)
+epd.text("clock-py",35,10,0x00)
+epd.display_Partial(epd.buffer)
+
 while (True):
-    try:
+    try: # main loop
+
+        # get time
         current_time = time.localtime()
-        year, month, day, hour, minute, sec, milsec, nanosec = current_time
-        print(f"h: {hour + 1} min: {minute} sec: {sec}")
-        sleep(1)
+        year, month, day, hour, minute, sec, milsec, nanosec = current_time 
+
+        #w rite buffer
+        epd.text(f"h: {hour + 1}",15,75,0x00)
+        epd.text(f"min: min:{minute}",15,75+12,0x00)
+        epd.text(f"sec: {sec}",15,75+12+12,0x00)
+        epd.display_Partial(epd.buffer)
+
+        # reset buffer
+        epd.text(f"h: {hour + 1}",15,75,0xff)
+        epd.text(f"min: min:{minute}",15,75+12,0xff)
+        epd.text(f"sec: {sec}",15,75+12+12,0xff)
+        
 
     except KeyboardInterrupt:
         print("bye")
